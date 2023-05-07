b1902baa9ef86874c7160fe3fecfa0eb10c54c1b
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-05-07 16:41:55 +0300

    install axios and get post from server

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="app">
+    <h1>Page with posts</h1>
+    <StandartButton @click="fetchPost">Get posts</StandartButton>
     <StandartButton @click="showDialog">Add post</StandartButton>
     <MyDialog v-model:show="dialogVisible">
       <PostForm @create="createPost" />
@@ -13,6 +15,7 @@
 import MyDialog from "@/components/UI/MyDialog.vue";
 import PostForm from "./../components/PostForm.vue";
 import PostList from "./../components/PostList.vue";
+import axios from "axios";
 
 export default {
   name: "HomeView",
@@ -23,9 +26,9 @@ export default {
   data() {
     return {
       posts: [
-        { id: 1, title: "JS", body: "Description posts" },
-        { id: 2, title: "Html", body: "Description posts" },
-        { id: 3, title: "Css", body: "Description posts" },
+        // { id: 1, title: "JS", body: "Description posts" },
+        // { id: 2, title: "Html", body: "Description posts" },
+        // { id: 3, title: "Css", body: "Description posts" },
       ],
       dialogVisible: false,
     };
@@ -41,11 +44,22 @@ export default {
     showDialog() {
       this.dialogVisible = true;
     },
+    async fetchPost() {
+      try {
+        const { data } = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+        );
+        this.posts = data;
+      } catch (error) {
+        alert("Error");
+        console.log(error.message);
+      }
+    },
   },
 };
 </script>
 
-<style scoped>
+<style>
 * {
   margin: 0;
   padding: 0;
